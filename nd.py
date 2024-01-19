0a42329d8981a929d2a6e4c704c07251c45a5803
Shinnosuke Kataoka <63338971+KATAOKA-Shinnosuke@users.noreply.github.com>
2024-01-19 16:49:08 +0900

    Update nd.py

--- nd.py
+++ nd.py
@@ -8,9 +8,23 @@ import sys
 import time
 
 b = BPF(src_file="nd.bpf.c")
-interface = "hogehoge"
+nic = sys.argv[1]
+f = b.load_func("xdp_nd", BPF.XDP)
 
-fx = b.load_func("xdp", BPF.XDP)
-BPF.attach_xdp(interface, fx, 0)
+b.attach_xdp(nic, f, 0)
 
-b.trace_print()
+prev_access = b.get_table("prev_access_hash")
+new_access = b.get_table("new_access_queue")
+while True:
+    try:
+        time.sleep(1)
+        os.system('clear')
+        for src, dst, count in prev_access.items():
+            print("({}, {}): {}".format(src.value, dst.value, count.value))
+        print("")
+        #for src, ts in new_access.get():
+        #    print("{} at {}".format(src.value, ts.value))
+    except KeyboardInterrupt:
+        break
+
+b.remove_xdp(nic)
