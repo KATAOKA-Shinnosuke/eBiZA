91a85a3da8699a6c2d9d455ae36fedd17a052321
Shinnosuke Kataoka <63338971+KATAOKA-Shinnosuke@users.noreply.github.com>
2024-01-20 22:08:25 +0900

    Implement logging and counting

--- nd.py
+++ nd.py
@@ -15,15 +15,26 @@ b.attach_xdp(nic, f, 0)
 
 prev_access = b.get_table("prev_access_hash")
 new_access = b.get_table("new_access_queue")
+scan_count = b.get_table("count_hash")
+#test_queue = b.get_table("test_queue")
+#test_hash = b.get_table("test_hash")
 while True:
     try:
-        time.sleep(1)
-        os.system('clear')
-        for src, dst, count in prev_access.items():
-            print("({}, {}): {}".format(src.value, dst.value, count.value))
-        print("")
-        #for src, ts in new_access.get():
-        #    print("{} at {}".format(src.value, ts.value))
+        time.sleep(5)
+        #os.system('clear')
+        for src_dst, count in prev_access.items():
+            print("({}, {}): {}".format(socket.inet_ntoa(src_dst.src_addr.to_bytes(4, byteorder='little')),
+                                        socket.inet_ntoa(src_dst.dst_addr.to_bytes(4, byteorder='little')), count.value))
+        #print(test_queue.values())
+        #print(test_hash.values())
+        #for src_ts in new_access.values():
+        #    print("{} at {}".format(socket.inet_ntoa(src_ts.src_addr.to_bytes(4, byteorder='little')),
+        #                            src_ts.timestamp))
+        #for src, scan in scan_count.items():
+        #    print("{}: {} scans".format(socket.inet_ntoa(int(src).to_bytes(4, byteorder='little')),
+        #                                scan))
+        #print(prev_access.values())
+        print(scan_count.values())
     except KeyboardInterrupt:
         break
 
